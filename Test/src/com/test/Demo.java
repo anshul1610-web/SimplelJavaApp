10b874c3cb7929ada0d590df4a7bfd765bdde143
anshu <anshu@LAPTOP-HONLTD3G>
2023-12-01 20:46:42 +0530

    test commit

---
+++ Test/src/com/test/Demo.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import java.util.Date;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello Simple Java Project");
+         System.out.println(new Date());
+         System.out.println("********************bdb******************************************");
+	}
+
+}
